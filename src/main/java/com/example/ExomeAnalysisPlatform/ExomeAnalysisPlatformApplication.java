d23905e4c058e986599dbfa06fed5e92e4c730f9
Horizon371 <45963302+Horizon371@users.noreply.github.com>
2021-02-21 20:15:46 +0200

    first commit 2

---
+++ src/main/java/com/example/ExomeAnalysisPlatform/ExomeAnalysisPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.ExomeAnalysisPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExomeAnalysisPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExomeAnalysisPlatformApplication.class, args);
+	}
+
+}
